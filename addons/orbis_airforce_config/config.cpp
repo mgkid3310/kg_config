b4154b8e795197eed849da7ffb8f263e7fc30f76
Orbis2358 <mgkid3310@naver.com>
2017-12-25 01:49:55 +0900

    pylon loadout config

--- addons/orbis_airforce_config/config.cpp
+++ addons/orbis_airforce_config/config.cpp
@@ -85,7 +85,13 @@ class CfgMagazines {
 		hardpoints[] = {"FIR_F16_Combined_HP", "FIR_F16_AG_HP", "B_BOMB_PYLON", "FIR_A10_BOMB_HP", "FIR_SH_AG_HP", "FIR_Aerobatics_Side", "FIR_Aerobatics_Wing"};
 	};
 	class FIR_SniperXR_1_P_1rnd_M: VehicleMagazine {
-		hardpoints[] = {"FIR_F16_Aux_HP", "FIR_Aerobatics_Aux", "FIR_SH_AUX_HP"};
+		hardpoints[] = {"FIR_F16_Aux_HP", "FIR_Aerobatics_Aux"};
+	};
+	class FIR_Litening_std_P_1rnd_M : VehicleMagazine {
+		hardpoints[] = {"B_BOMB_PYLON", "FIR_A10_AUX_HP", "FIR_SH_AUX_HP"};
+	};
+	class FIR_SniperXR_2_P_1rnd_M : VehicleMagazine {
+		hardpoints[] = {"B_BOMB_PYLON", "FIR_A10_AUX_HP", "FIR_SH_AUX_HP"};
 	};
 
 	class FIR_AIM120_P_type1_2rnd_M : VehicleMagazine {
