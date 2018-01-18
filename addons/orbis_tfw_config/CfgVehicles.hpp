8b7156ba1a19c6c20eaafcfaaf4fd8de72f39087
mgkid3310 <mgkid3310@naver.com>
2018-01-19 00:04:49 +0900

    show basic backpacks only

--- addons/orbis_tfw_config/CfgVehicles.hpp
+++ addons/orbis_tfw_config/CfgVehicles.hpp
@@ -21,22 +21,23 @@ class CfgVehicles {
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Digi_Backpack_Radio_IA.paa"};
 	};
 	class tfw_roka_arctic: tfw_roka_digi{
+		scope = 1;
 		displayName = "ROKA RT-1523G (ASIP) Arctic";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Arctic_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_desert: tfw_roka_digi{
+	class tfw_roka_desert: tfw_roka_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Desert";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Desert_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_frog: tfw_roka_digi{
+	class tfw_roka_frog: tfw_roka_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Frog";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Frog_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_mc: tfw_roka_digi{
+	class tfw_roka_mc: tfw_roka_arctic{
 		displayName = "ROKA RT-1523G (ASIP) MC";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_MC_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_spec: tfw_roka_digi{
+	class tfw_roka_spec: tfw_roka_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Spec";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Spec_Backpack_Radio_IA.paa"};
 	};
@@ -48,22 +49,23 @@ class CfgVehicles {
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Digi_Backpack_Radio_IA.paa"};
 	};
 	class tfw_roka_a_arctic: tfw_roka_a_digi{
+		scope = 1;
 		displayName = "ROKA RT-1523G (ASIP) Arctic ANT";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Arctic_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_a_desert: tfw_roka_a_digi{
+	class tfw_roka_a_desert: tfw_roka_a_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Desert ANT";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Desert_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_a_frog: tfw_roka_a_digi{
+	class tfw_roka_a_frog: tfw_roka_a_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Frog ANT";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Frog_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_a_mc: tfw_roka_a_digi{
+	class tfw_roka_a_mc: tfw_roka_a_arctic{
 		displayName = "ROKA RT-1523G (ASIP) MC ANT";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_MC_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_a_spec: tfw_roka_a_digi{
+	class tfw_roka_a_spec: tfw_roka_a_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Spec ANT";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Spec_Backpack_Radio_IA.paa"};
 	};
@@ -75,22 +77,23 @@ class CfgVehicles {
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Digi_Backpack_Radio_IA.paa"};
 	};
 	class tfw_roka_dd_arctic: tfw_roka_dd_digi{
+		scope = 1;
 		displayName = "ROKA RT-1523G (ASIP) Arctic VHF/ UHF Antenna";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Arctic_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_dd_desert: tfw_roka_dd_digi{
+	class tfw_roka_dd_desert: tfw_roka_dd_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Desert VHF/ UHF Antenna";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Desert_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_dd_frog: tfw_roka_dd_digi{
+	class tfw_roka_dd_frog: tfw_roka_dd_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Frog VHF/ UHF Antenna";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Frog_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_dd_mc: tfw_roka_dd_digi{
+	class tfw_roka_dd_mc: tfw_roka_dd_arctic{
 		displayName = "ROKA RT-1523G (ASIP) MC VHF/ UHF Antenna";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_MC_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_dd_spec: tfw_roka_dd_digi{
+	class tfw_roka_dd_spec: tfw_roka_dd_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Spec VHF/ UHF Antenna";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Spec_Backpack_Radio_IA.paa"};
 	};
@@ -102,22 +105,23 @@ class CfgVehicles {
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Digi_Backpack_Radio_IA.paa"};
 	};
 	class tfw_roka_blade_arctic: tfw_roka_blade_digi{
+		scope = 1;
 		displayName = "ROKA RT-1523G (ASIP) Arctic VHF Blade Antenna";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Arctic_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_blade_desert: tfw_roka_blade_digi{
+	class tfw_roka_blade_desert: tfw_roka_blade_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Desert VHF Blade Antenna";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Desert_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_blade_frog: tfw_roka_blade_digi{
+	class tfw_roka_blade_frog: tfw_roka_blade_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Frog VHF Blade Antenna";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Frog_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_blade_mc: tfw_roka_blade_digi{
+	class tfw_roka_blade_mc: tfw_roka_blade_arctic{
 		displayName = "ROKA RT-1523G (ASIP) MC VHF Blade Antenna";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_MC_Backpack_Radio_IA.paa"};
 	};
-	class tfw_roka_blade_spec: tfw_roka_blade_digi{
+	class tfw_roka_blade_spec: tfw_roka_blade_arctic{
 		displayName = "ROKA RT-1523G (ASIP) Spec VHF Blade Antenna";
 		hiddenSelectionsTextures[] = {"orbis_tfw_config\textures\ROKA_D_Spec_Backpack_Radio_IA.paa"};
 	};
