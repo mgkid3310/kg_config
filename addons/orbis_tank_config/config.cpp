2d99c6eb22040e07aca1724eac067a2d20430b31
mgkid3310 <mgkid3310@naver.com>
2018-02-08 03:55:56 +0900

    cannon overpressure range reduced

--- addons/orbis_tank_config/config.cpp
+++ addons/orbis_tank_config/config.cpp
@@ -3,13 +3,33 @@ class CfgPatches {
 		name = "Orbis Tank Config";
 		author = "Orbis2358";
 		requiredVersion = 0.1;
-		requiredAddons[] = {"RHS_US_A2Port_Armor", "rhsusf_c_m1a2", "rhsusf_c_m113"};
+		requiredAddons[] = {"ace_overpressure", "RHS_US_A2Port_Armor", "rhsusf_c_m1a2", "rhsusf_c_m113"};
 		units[] = {};
 		weapons[] = {};
 	};
 };
 
 #include "CfgEventHandlers.hpp"
+
+class CfgWeapons {
+	class CannonCore;
+	class cannon_120mm: CannonCore {
+		ace_overpressure_range = 0.1;
+	};
+
+	class cannon_125mm: CannonCore {
+		ace_overpressure_range = 0.1;
+	};
+
+	class cannon_105mm: CannonCore {
+		ace_overpressure_range = 0.1;
+	};
+
+	class mortar_155mm_AMOS: CannonCore {
+		ace_overpressure_range = 0.1;
+	};
+};
+
 #include "CfgVehicles.hpp"
 
 class CfgSounds {
