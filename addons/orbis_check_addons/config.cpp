b1ffeb243fdd6a8f019ea844621d9a292a7e6193
mgkid3310 <mgkid3310@naver.com>
2018-02-21 17:02:35 +0900

    check & alert whitelist/balcklist addons

---
+++ addons/orbis_check_addons/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+	class orbis_check_addons {
+		name = "Orbis Check Addons";
+		author = "Orbis2358";
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+#include "CfgEventHandlers.hpp"
