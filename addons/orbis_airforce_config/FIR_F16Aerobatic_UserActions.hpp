d79769ab4753a23da698fe6ed20a34669f8eeb5f
mgkid3310 <mgkid3310@naver.com>
2018-04-10 01:10:15 +0900

    smoke generator modified

--- addons/orbis_airforce_config/FIR_F16Aerobatic_UserActions.hpp
+++ addons/orbis_airforce_config/FIR_F16Aerobatic_UserActions.hpp
@@ -7,28 +7,28 @@
 				showWindow = 1;
 				hideOnUse = 1;
 				radius = 5;
-				condition = "(ACE_player in this) && (this getVariable ['smokeMod', 'off'] != 'off')";
+				condition = "(ACE_player in this) && (isEngineOn this) && (this getVariable ['smokeReady', true]) && (this getVariable ['smokeMod', 'off'] != 'off')";
 				statement = "hintSilent 'Smoke Off'; ['toggleSmoke', [this, 'off', [0, 0, 0, 0]]] call CBA_fnc_globalEvent";
 			};
 			class smoke_on_white: smoke_off {
 				displayName = "Turn on White Smoke";
 				showWindow = 0;
-				condition = "(ACE_player in this) && (this getVariable ['smokeMod', 'off'] != 'white')";
+				condition = "(ACE_player in this) && (isEngineOn this)  && (this getVariable ['smokeReady', true]) && (this getVariable ['smokeMod', 'off'] != 'white')";
 				statement = "hintSilent 'White Smoke'; ['toggleSmoke', [this, 'white', [1, 1, 1, 0.15]]] call CBA_fnc_globalEvent";
 			};
 			class smoke_on_red: smoke_on_white {
 				displayName = "Turn on Red Smoke";
-				condition = "(ACE_player in this) && (this getVariable ['smokeMod', 'off'] != 'red')";
+				condition = "(ACE_player in this) && (isEngineOn this)  && (this getVariable ['smokeReady', true]) && (this getVariable ['smokeMod', 'off'] != 'red')";
 				statement = "hintSilent 'Red Smoke'; ['toggleSmoke', [this, 'red', [0.8, 0.0, 0.0, 0.15]]] call CBA_fnc_globalEvent";
 			};
 			/* class smoke_on_green: smoke_on_white {
 				displayName = "Turn on Green Smoke";
-				condition = "(ACE_player in this) && (this getVariable ['smokeMod', 'off'] != 'green')";
+				condition = "(ACE_player in this) && (isEngineOn this)  && (this getVariable ['smokeReady', true]) && (this getVariable ['smokeMod', 'off'] != 'green')";
 				statement = "hintSilent 'Green Smoke'; ['toggleSmoke', [this, 'green', [0.0, 0.8, 0.0, 0.15]]] call CBA_fnc_globalEvent";
 			}; */
 			class smoke_on_blue: smoke_on_white {
 				displayName = "Turn on Blue Smoke";
-				condition = "(ACE_player in this) && (this getVariable ['smokeMod', 'off'] != 'blue')";
+				condition = "(ACE_player in this) && (isEngineOn this)  && (this getVariable ['smokeReady', true]) && (this getVariable ['smokeMod', 'off'] != 'blue')";
 				statement = "hintSilent 'Blue Smoke'; ['toggleSmoke', [this, 'blue', [0.0, 0.0, 0.8, 0.15]]] call CBA_fnc_globalEvent";
 			};
 		};
