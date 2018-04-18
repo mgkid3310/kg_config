16e403a6b5b72eb4cc76dbd65fdb8e61ea632881
mgkid3310 <mgkid3310@naver.com>
2018-04-18 23:58:04 +0900

    add cbrn addon basework

---
+++ addons/orbis_cbrn_weapons/config.cpp
@@ -0,0 +1,12 @@
+class CfgPatches {
+	class orbis_cbrn_weapons {
+		name = "Orbis CBRN Weapons";
+		author = "Orbis2358";
+		requiredVersion = 0.1;
+		requiredAddons[] = {};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+// #include "CfgEventHandlers.hpp"
