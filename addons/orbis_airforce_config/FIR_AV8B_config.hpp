0bc1eea8095f6d8063f1f26b1f46da5705b15722
mgkid3310 <mgkid3310@naver.com>
2018-04-25 00:19:36 +0900

    av-8b harrier config added

---
+++ addons/orbis_airforce_config/FIR_AV8B_config.hpp
@@ -0,0 +1,50 @@
+	class FIR_AV8B_Base: Plane_Fighter_03_base_F {
+		receiveRemoteTargets = true;
+		reportRemoteTargets = true;
+		reportOwnPosition = true;
+		vtol = 1;
+
+		class UserActions {
+			// Todo
+		};
+
+		class EventHandlers {
+			class orbis_config {
+				init = "";
+			};
+		};
+	};
+
+    class FIR_AV8B_NA_Base: Plane_Fighter_03_base_F {
+		receiveRemoteTargets = true;
+		reportRemoteTargets = true;
+		reportOwnPosition = true;
+		vtol = 1;
+
+		class UserActions {
+			// Todo
+		};
+
+		class EventHandlers {
+			class orbis_config {
+				init = "";
+			};
+		};
+	};
+
+    class FIR_AV8B_GR7_Base: Plane_Fighter_03_base_F {
+		receiveRemoteTargets = true;
+		reportRemoteTargets = true;
+		reportOwnPosition = true;
+		vtol = 1;
+
+		class UserActions {
+			// Todo
+		};
+
+		class EventHandlers {
+			class orbis_config {
+				init = "";
+			};
+		};
+	};
